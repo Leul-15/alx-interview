79c0d457efc7748a1dd2e0f39979a6e0a5a36e58
Leul <Leulassefa255@gmail.com>
2024-05-23 09:11:54 -0700

    Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,23 +1,18 @@
 #!/usr/bin/node
+const util = require('util');
+const request = util.promisify(require('request'));
+const filmID = process.argv[2];
 
-const request = require('request');
+async function starwarsCharacters (filmId) {
+  const url = 'https://swapi-api.alx-tools.com/api/films/' + filmId;
+  let response = await (await request(url)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
 
-const movieId = process.argv[2];
-
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request(url, async (err, res, body) => {
-  err && console.log(err);
-
-  const charactersArray = (JSON.parse(res.body).characters);
-  for (const character of charactersArray) {
-    await new Promise((resolve, reject) => {
-      request(character, (err, res, body) => {
-        err && console.log(err);
-
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
-    });
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
   }
-});
+}
